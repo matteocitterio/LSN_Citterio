71d71644b89d3ddfd157570f2a2fa8315e30b71b
matteocitterio <72971538+matteocitterio@users.noreply.github.com>
2023-07-09 14:28:43 +0200

    Add files via upload

--- LSN_2/2.1/functions.cpp
+++ LSN_2/2.1/functions.cpp
@@ -2,29 +2,45 @@
 
 using namespace std;
 
-//Cosine constructors
 Cosine::Cosine(){
+
+    /*
+    Cosine empty constructor
+    */
+
     m_a = 1;
     m_b = 1;
     m_c = 0;
 }
 
 Cosine::Cosine(double a, double b, double c){
+
+    /*
+    Cosine constructor
+    */
+
     m_a = a;
     m_b = b;
     m_c = c;
 }
 
-// Parabola constructors
-Parabola::Parabola()
-{
+Parabola::Parabola(){
+
+    /*
+    Parabola empty constructor
+    */
+
     m_a = 1;
     m_b = 1;
     m_c = 1;
 }
 
-Parabola::Parabola(double a, double b, double c)
-{
+Parabola::Parabola(double a, double b, double c){
+
+    /*
+    Parabola constructor
+    */
+
     m_a = a;
     m_b = b;
     m_c = c;
--- LSN_2/2.1/functions.h
+++ LSN_2/2.1/functions.h
@@ -5,29 +5,36 @@
 
 using namespace std;
 
-//Parent class used for every basic function
 class Functions {
 
+    /*
+    Parent class used for every basic function
+    */
+
     public:
-        virtual double Evaluate (double x) const =0;        //by defining it virtual, we make sure that every child class needs to implement this method
+        virtual double Evaluate (double x) const =0;                                    // by defining it virtual, we make sure that every child class needs to implement this method
 
 };
 
-//class cosine
 class Cosine: public Functions {
+
+    /*
+    Derived from Functions, it build a Cosine obj.
+    */
+
     private:
         double m_a;
         double m_b;
         double m_c;
 
     public:
-        Cosine();      //empty constructor
-        Cosine(double a, double b, double c);
-        ~Cosine() {;};  //empty destructor
+        Cosine();                                                                      // empty constructor
+        Cosine(double a, double b, double c);                                          // Constructor
+        ~Cosine() {;};                                                                 // Empty destructor
 
-        virtual double Evaluate (double x) const {return m_a * cos(m_b * x + m_c);}    //implementation of the virtual method
+        virtual double Evaluate (double x) const {return m_a * cos(m_b * x + m_c);}    // implementation of the virtual method
 
-        void SetA(double a) { m_a = a; }
+        void SetA(double a) { m_a = a; }                                                
         void SetB(double b) { m_b = b; }
         void SetC(double c) { m_c = c; }
         double GetA() { return m_a; }
@@ -35,18 +42,21 @@ class Cosine: public Functions {
         double GetC() { return m_c; }
 };
 
-// class parabola
-class Parabola : public Functions
-{
+class Parabola : public Functions {
+
+    /*
+    Derived from Functions, it build a Parabola obj.
+    */
+
     private:
         double m_a;
         double m_b;
         double m_c;
 
     public:
-        Parabola(); // empty constructor
-        Parabola(double a, double b, double c);
-        ~Parabola() { ; }; // empty destructor
+        Parabola();                                                                     // empty constructor
+        Parabola(double a, double b, double c);                                         // constructor
+        ~Parabola() { ; };                                                              // empty destructor
 
         virtual double Evaluate(double x) const { return m_a * pow(x,2) + m_b * x + m_c; } // implementation of the virtual method
 
