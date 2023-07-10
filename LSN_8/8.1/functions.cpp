ca64c465a947d6e24f84e8306c23da712dc62811
matteocitterio <72971538+matteocitterio@users.noreply.github.com>
2023-07-10 16:59:50 +0200

    Add files via upload

--- LSN_8/8.1/functions.cpp
+++ LSN_8/8.1/functions.cpp
@@ -2,22 +2,37 @@
 
 using namespace std;
 
-//Cosine constructors
-Cosine::Cosine(){
+Cosine::Cosine()
+{
+
+    /*
+    Cosine empty constructor
+    */
+
     m_a = 1;
     m_b = 1;
     m_c = 0;
 }
 
-Cosine::Cosine(double a, double b, double c){
+Cosine::Cosine(double a, double b, double c)
+{
+
+    /*
+    Cosine constructor
+    */
+
     m_a = a;
     m_b = b;
     m_c = c;
 }
 
-// Parabola constructors
 Parabola::Parabola()
 {
+
+    /*
+    Parabola empty constructor
+    */
+
     m_a = 1;
     m_b = 1;
     m_c = 1;
@@ -25,18 +40,33 @@ Parabola::Parabola()
 
 Parabola::Parabola(double a, double b, double c)
 {
+
+    /*
+    Parabola constructor
+    */
+
     m_a = a;
     m_b = b;
     m_c = c;
 }
 
-//Hydrogen Ground State constructors
 HydrogenGS::HydrogenGS()
 {
+
+    /*
+    Hydrogen GS constructor
+    */
+
     m_a0 = 0.0529 * pow(10, -9);
 }
+
 Hydrogen210::Hydrogen210()
 {
+
+    /*
+    Hydrogen excited state constuctor
+    */
+
     m_a0 = 0.0529 * pow(10, -9);
 }
 
@@ -47,23 +77,39 @@ TrialWaveFunction::TrialWaveFunction(){
     m_sigma = 1.;
 }
 
-//Evaluate function
 double Hydrogen210::Evaluate(arma::vec v) const {
+
+    /*
+    Evaluation method for the excited state of the Hydrogen pdf.
+    */
+
     double theta = acos(v[2]/norm(v,2));
     return pow(m_a0, -5) / ( 32 * M_PI ) * pow(arma::norm(v, 2), 2) * exp( - arma::norm(v,2) / m_a0 ) * pow( cos(theta) , 2);
 }
 
 double TrialWaveFunction::Evaluate(double x) const {
+
+    /*
+    Evaluation method for the trial wafe function pdf.
+    */
+
     return pow(abs(exp(-pow(x - m_mu, 2) / (2 * pow(m_sigma, 2))) + exp(-pow(x + m_mu, 2) / (2 * pow(m_sigma, 2)))), 2);
 }
 
-//This is the evalutaion without the modulus
-double TrialWaveFunction::EvaluateNoModulus(double x) const
-{
+double TrialWaveFunction::EvaluateNoModulus(double x) const {
+
+    /*
+    This is the evalutaion without the modulus
+    */
+
     return abs(exp(-pow(x - m_mu, 2) / (2 * pow(m_sigma, 2))) + exp(-pow(x + m_mu, 2) / (2 * pow(m_sigma, 2))));
 }
 
 double TrialWaveFunction::SecondDerivative(double x) const{
+
+    /*
+    Implementation of the second derivative used in the variational Montecarlo code for the Trial wave function
+    */
 
     double minusExp = exp(-0.5 * (pow(x - m_mu, 2) / (pow(m_sigma, 2))));
     double plusExp = exp(-0.5 * (pow(x + m_mu, 2) / (pow(m_sigma, 2))));
@@ -72,17 +118,31 @@ double TrialWaveFunction::SecondDerivative(double x) const{
 }
 
 DoubleDwellPotential::DoubleDwellPotential(){
+
+    /*
+    Constructor of the dwell-shaped potential
+    */
+
     m_a = -2.5;
     m_b = 1;
 }
 
 double DoubleDwellPotential::Evaluate(double x) const {
 
+    /*
+    Evaluation method for the dwell-shaped potential
+    */
+
     return m_a * pow(x, 2) + m_b * pow(x,4);
 
 }
 
 void DoubleDwellPotential::SetCoeffs(arma::vec coeff){
+
+    /*
+    Simple set method
+    */
+
     m_a = coeff[0];
     m_b = coeff[1];
 }
--- LSN_8/8.1/functions.h
+++ LSN_8/8.1/functions.h
@@ -7,27 +7,34 @@
 using namespace std;
 using namespace arma;
 
-//Parent class used for every basic function
 class Functions {
 
+    /*
+    Parent class used for every basic function
+    */
+
     public:
-        virtual double Evaluate (double x) const =0;        //by defining it virtual, we make sure that every child class needs to implement this method
+        virtual double Evaluate (double x) const =0;                                  // By defining it virtual, we make sure that every child class needs to implement this method
         virtual double Evaluate (arma::vec v) const = 0;
 };
 
-//class cosine
 class Cosine: public Functions {
+
+    /*
+    Derived from Functions, it builds a Cosine obj.
+    */
+
     private:
         double m_a;
         double m_b;
         double m_c;
 
     public:
-        Cosine();      //empty constructor
-        Cosine(double a, double b, double c);
-        ~Cosine() {;};  //empty destructor
+        Cosine();                                                                     // empty constructor
+        Cosine(double a, double b, double c);                                         // Constructor
+        ~Cosine() {;};                                                                //empty destructor
 
-        virtual double Evaluate (double x) const {return m_a * cos(m_b * x + m_c);}    //implementation of the virtual method
+        virtual double Evaluate (double x) const {return m_a * cos(m_b * x + m_c);}   // Implementation of the virtual method
 
         void SetA(double a) { m_a = a; }
         void SetB(double b) { m_b = b; }
@@ -37,18 +44,21 @@ class Cosine: public Functions {
         double GetC() { return m_c; }
 };
 
-// class parabola
-class Parabola : public Functions
-{
+class Parabola : public Functions {
+
+    /*
+    Derived from Functions, it builds a Parabola obj.
+    */
+
     private:
         double m_a;
         double m_b;
         double m_c;
 
     public:
-        Parabola(); // empty constructor
-        Parabola(double a, double b, double c);
-        ~Parabola() { ; }; // empty destructor
+        Parabola();                                                                   // empty constructor
+        Parabola(double a, double b, double c);                                       // Constructor
+        ~Parabola() { ; };                                                            // empty destructor
 
         virtual double Evaluate(double x) const { return m_a * pow(x,2) + m_b * x + m_c; } // implementation of the virtual method
 
@@ -60,12 +70,15 @@ class Parabola : public Functions
         double GetC() { return m_c; }
 };
 
-//class Hydrogen Ground State
-class HydrogenGS: public Functions 
-{
+class HydrogenGS: public Functions {
+
+    /*
+    Derived from Functions, it builds a Hydrogen Ground State obj.
+    */
+
     public:
-        HydrogenGS(); // empty constructor
-        ~HydrogenGS() { ; }; // empty destructor
+        HydrogenGS();                                                                 // empty constructor
+        ~HydrogenGS() { ; };                                                          // empty destructor
 
         virtual double Evaluate(arma::vec v) const { return pow(m_a0, -3) * exp(-2 * (arma::norm(v,2)) / m_a0) / M_PI; }
         virtual double Evaluate(double x) const
@@ -81,11 +94,15 @@ class HydrogenGS: public Functions
         double m_a0;
 };
 
-class Hydrogen210: public Functions
-{
+class Hydrogen210: public Functions {
+
+    /*
+    Derived from Functions, it builds a Hydrogen Excited State obj.
+    */
+
     public:
-        Hydrogen210();        // empty constructor
-        ~Hydrogen210() { ; }; // empty destructor
+        Hydrogen210();                                                                // empty constructor
+        ~Hydrogen210() { ; };                                                         // empty destructor
 
         virtual double Evaluate(arma::vec v) const;
         virtual double Evaluate(double v) const {
@@ -101,18 +118,22 @@ class Hydrogen210: public Functions
 
 class TrialWaveFunction: public Functions {
 
+    /*
+    Derived from functions, it build a trial wave function obj used for the Variational montecarlo code.
+    */
+
     public: 
-        TrialWaveFunction();        //empty constructor
-        ~TrialWaveFunction() {;};   //empty destructor
+        TrialWaveFunction();                                                          // empty constructor
+        ~TrialWaveFunction() {;};                                                     // empty destructor
 
         virtual double Evaluate(arma::vec v) const
         {
             cout << "This WF is for a 1D model, hence it works with doubles not vectors. There is something wrong" << endl;
             return 0;
         };
-        virtual double Evaluate(double x) const;
-        double EvaluateNoModulus(double x) const;
-        double SecondDerivative(double x) const;
+        virtual double Evaluate(double x) const;                                      // Evaluation method
+        double EvaluateNoModulus(double x) const;                                     // Evaluation without modulus
+        double SecondDerivative(double x) const;                                      // Second derivative
         void Set_bohr_radius(double a) { m_a0 = a; }
         void Set_Mu(double mu) {m_mu = mu;}
         void Set_Sigma (double sigma) {m_sigma = sigma;}
@@ -127,15 +148,19 @@ class TrialWaveFunction: public Functions {
 
 class DoubleDwellPotential: public Functions {
 
+    /*
+    Derived from FUnctions, it build a dwell-shape potential obj.
+    */
+
     public:
-        DoubleDwellPotential();        //empty constructor
-        ~DoubleDwellPotential() {;};   //empty destructor
+        DoubleDwellPotential();                                                       // empty constructor
+        ~DoubleDwellPotential() {;};                                                  // empty destructor
 
         virtual double Evaluate(arma::vec v) const{
             cout << "The potential is 1D, please give a 1D INPUT, not a VECTOR" << endl;
             return 0;
         };
-        virtual double Evaluate(double x) const;
+        virtual double Evaluate(double x) const;                                      // Evaluation
         void SetCoeffs(arma::vec coeff);
 
     private:
