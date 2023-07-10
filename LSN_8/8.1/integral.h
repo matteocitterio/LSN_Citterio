bca0519d1020d7efa4b401a6e0713f01b13a1a0e
matteocitterio <72971538+matteocitterio@users.noreply.github.com>
2023-07-10 17:03:07 +0200

    Add files via upload

--- LSN_8/8.1/integral.h
+++ LSN_8/8.1/integral.h
@@ -10,18 +10,22 @@
 using namespace std;
 
 class Integral {
+
+    /*
+    This class contains all the methods used for computing the integral of a `Functions` object
+    */
     
     public:
-        Integral(double a, double b, Functions* f, Random* rnd);
-        double arithAverage (unsigned int N);
-        double Imp_sampling (unsigned int N, double d_max, Functions* d);
+        Integral(double a, double b, Functions* f, Random* rnd);                        // Constructor
+        double arithAverage (unsigned int N);                                           // Uniform sampling
+        double Imp_sampling (unsigned int N, double d_max, Functions* d);               // Importance sampling
 
     private:
-        double I_min;
+        double I_min;                                                                   // integral domain
         double I_max;
-        double I_integral;
-        double I_sign;
-        Functions* I_f;
+        double I_integral;                                                              // used to store the integral value
+        double I_sign;                                                                  // '' the ingral sign
+        Functions* I_f;                                                                 
         Random* I_rnd;
 };
 
