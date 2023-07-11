943fd82f10fd9f47aa9396ba9a0f9090e563567e
matteocitterio <72971538+matteocitterio@users.noreply.github.com>
2023-07-11 15:12:28 +0200

    Add files via upload

--- LSN_8/8.2/main.cpp
+++ LSN_8/8.2/main.cpp
@@ -13,9 +13,9 @@ using namespace arma;
 
 int main(int argc, char *argv[]){
 
-    //Settings for the Random generator class
-    Random* rnd = new Random();
-    rnd -> RandomRoutine();   //Random settings including seed etc
+    // Initialize an element of the Random class
+    Random *rnd = new Random();
+    rnd->RandomRoutine();                                                           // Random settings including setting the seed, ....
 
     // M thows, N blocks, L throws in each block
     int M = pow(10,6);    
@@ -27,9 +27,10 @@ int main(int argc, char *argv[]){
     //temperature
     double T = 2.;
 
-    TrialWaveFunction *TWF = new TrialWaveFunction(rnd);       // my trial wave function
-    DoubleDwellPotential *DDP = new DoubleDwellPotential(); // my potential term of the Hamiltonian
-    Annealing *ANN = new Annealing(TWF, rnd, DDP);             // used for the SA scheme
+    // Build pointers to the objects I need in the SA schema
+    TrialWaveFunction *TWF = new TrialWaveFunction(rnd);                            // my trial wave function
+    DoubleDwellPotential *DDP = new DoubleDwellPotential();                         // my potential term of the Hamiltonian
+    Annealing *ANN = new Annealing(TWF, rnd, DDP);                                  // used for the SA scheme
 
     //Set the cycle variables:
     ANN->SetN(N);
@@ -47,22 +48,23 @@ int main(int argc, char *argv[]){
     Averages << T << " " << ANN->GetEnergy() << " " << ANN->GetError() << " " << ANN-> GetEnergy()<< endl;
     Parameters << T << " " << ANN->GetMu() << " " << ANN->GetSigma() << endl;
 
+    // Auxiliary variables that I will use to output the optimized set of parameters
     double bestEnergy = ANN->GetEnergy();
     arma::vec bestParameters { ANN->GetMu(), ANN->GetSigma()};
 
-    while (T >= 0.01){
+    while (T >= 0.01) {                                                             // Loop over the temperature decreasing (annealing)
 
         cout << "T: " << T << endl;
 
-        //Updates TWF parameters and computes the new energy, if it is lower, it saves the new configuration
-        ANN->MetropolisAnnealing();   //it includes equilibration
+        //Updates TWF parameters and computes the new energy, if it is accepted by metropolis, it saves the new configuration
+        ANN->MetropolisAnnealing();                                                 // it includes equilibration for the TWF with the new set of parameters
         Averages << T << " " << ANN->GetEnergy() << " " << ANN->GetError() << " " << bestEnergy << endl;
         Parameters << T << " " << ANN->GetMu() << " " << ANN->GetSigma() << endl;
         
-        //Update temperature
+        // Update temperature 
         T = T * 0.997;
         ANN->SetBeta(1/T);
-        if (bestEnergy > ANN->GetEnergy()) {
+        if (bestEnergy > ANN->GetEnergy()) {                                        // If a better configuration is found, save it
             bestEnergy = ANN->GetEnergy();
             bestParameters = { ANN->GetMu(),ANN->GetSigma() };
         }
@@ -75,13 +77,13 @@ int main(int argc, char *argv[]){
     Averages.close();
     Parameters.close();
 
+    // The SA scheme is concluded, now that I identified an optimized set of parameters, i compute the corresponding energy with data blocking and a wider statistics
     Averages.open("OptimizedEnergy.txt");
     Coordinates.open("OptimizedCoordinates.txt");
 
     cout << "Finding the optimized energy" << endl;
 
-    //Setting the optimized parameters
-    TWF->Set_Mu(bestParameters[0]);
+    TWF->Set_Mu(bestParameters[0]);                                                 // Setting the optimized parameters
     TWF->Set_Sigma(bestParameters[1]);
 
     double runningSum = 0.;
@@ -89,23 +91,23 @@ int main(int argc, char *argv[]){
     double error = 0.;
     double integral = 0.;
 
-    TWF->Equilibrate(100, pow(10, 3));
+    TWF->Equilibrate(100, pow(10, 3));                                              // Equilibrate the system before Metropolis
 
-    for (int i = 0; i < N; i++)
-    {
+    for (int i = 0; i < N; i++) {                                                   // Loop over the blocks
+
         integral = 0;
 
-        for (int j = 0; j < L; j++)
-        {
+        for (int j = 0; j < L; j++) {                                               // Loop over the throws
 
-            // updates my position according to my trial wave function distribution
-            TWF->MetropolisUniform();
+            TWF->MetropolisUniform();                                               // Evolve the position through metropolis
             // Now we need to compute the energy
             integral += ((-0.5 * TWF->SecondDerivative(TWF->Get_Position())) / TWF->EvaluateNoModulus(TWF->Get_Position())) + DDP->Evaluate(TWF->Get_Position());
             Coordinates << TWF->Get_Position() << endl;
         }
+
+        // Data blocking - I/O managment
         integral /= L;
-        runningSum = ((runningSum * i) + integral) / (i + 1); // at every iteration I have to re-multiplicate the previous division
+        runningSum = ((runningSum * i) + integral) / (i + 1);                       // at every iteration I have to re-multiplicate the previous division
         runningSquared = ((runningSquared * i) + pow(integral, 2)) / (i + 1);
         error = Error(runningSum, runningSquared, i);
         Averages << i << " " << runningSum << " " << error << endl;
