f5127a70267a965bd73c16d06302c2381419ee93
matteocitterio <72971538+matteocitterio@users.noreply.github.com>
2023-07-11 09:42:11 +0200

    Add files via upload

--- LSN_8/8.1/main.cpp
+++ LSN_8/8.1/main.cpp
@@ -12,9 +12,9 @@ using namespace arma;
 
 int main(int argc, char *argv[]){
 
-    //Settings for the Random generator class
-    Random* rnd = new Random();
-    rnd -> RandomRoutine();   //Random settings including seed etc
+    // Initialize an element of the Random class
+    Random *rnd = new Random();
+    rnd->RandomRoutine();                                                           // Random settings including setting the seed, ....
 
     // M thows, N blocks, L throws in each block
     int M = pow(10,6);    
@@ -23,27 +23,27 @@ int main(int argc, char *argv[]){
 
     cout << "Working with " << L << " throws in each block" << endl;
 
-    //variables for acceptance rate of the algorithm
+    // variables for acceptance rate of the algorithm
     double accepted = 0.;
     double attempted = 0.;
 
-    //Define global variables
-    double a0 = 0.0529 * pow(10, -9); // reduced units
-    double c = 2 ;       //to tune the acceptance rate
+    // Define global variables
+    double a0 = 0.0529 * pow(10, -9);                                               // reduced units
+    double c = 2 ;                                                                  // to tune the acceptance rate
     double integral = 0;             
 
-    //Blocking average variables
+    // Blocking average variables
     double runningSum = 0.;
     double runningSquared = 0.;
     double error = 0.;
 
-    //File managment
+    // File managment
     ofstream Averages;
     ofstream Coordinates;
 
-    double x = 0;       //my position starting point
-    TrialWaveFunction *TWF = new TrialWaveFunction();   //my trial wave function
-    DoubleDwellPotential *DDP = new DoubleDwellPotential();  //my potential term of the Hamiltonian
+    double x = 0;                                                                   // my position starting point
+    TrialWaveFunction *TWF = new TrialWaveFunction();                               // my trial wave function
+    DoubleDwellPotential *DDP = new DoubleDwellPotential();                         // my potential term of the Hamiltonian
 
     //Open output files
     Averages.open("Averages0.txt");
@@ -60,21 +60,24 @@ int main(int argc, char *argv[]){
     EquilibrateUN(100, pow(10,3), x, TWF, rnd, c);
     cout << "----------------------------" << endl;
 
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < N; i++) {                                                   // Loop over the blocks
+
         integral = 0;
         accepted = 0;
         attempted = 0;
-        for (int j = 0; j < L; j++){           
 
-                //updates my position according to my trial wave function distribution
-                MetropolisUniform(x, TWF, rnd, c, accepted, attempted);
-                //Now we need to compute the energy
-                integral += (( -0.5 * TWF->SecondDerivative(x) ) / TWF->EvaluateNoModulus(x))  + DDP->Evaluate(x);  //Energy expectation
-                Coordinates << x << endl;
+        for (int j = 0; j < L; j++){                                                // Loop ovewr the throws       
+
+            MetropolisUniform(x, TWF, rnd, c, accepted, attempted);                 // Updates my position according to my trial wave function distribution
+            //Now we need to compute the energy
+            integral += (( -0.5 * TWF->SecondDerivative(x) ) / TWF->EvaluateNoModulus(x))  + DDP->Evaluate(x);  //Energy expectation
+            Coordinates << x << endl;
         }
+
+        // Data - blocking
         integral /= L; 
 
-        runningSum = ((runningSum * i) + integral) / (i + 1);                  // at every iteration I have to re-multiplicate the previous division
+        runningSum = ((runningSum * i) + integral) / (i + 1);                       // at every iteration I have to re-multiplicate the previous division
         runningSquared = ((runningSquared * i) + pow(integral, 2)) / (i + 1);
         error = Error(runningSum, runningSquared, i);
         Averages << i <<" "<< runningSum << " " << error << endl;
