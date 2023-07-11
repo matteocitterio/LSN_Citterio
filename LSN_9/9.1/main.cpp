64454b3142a90022175bf380ff38299ea660423f
matteocitterio <72971538+matteocitterio@users.noreply.github.com>
2023-07-11 15:59:26 +0200

    Add files via upload

--- LSN_9/9.1/main.cpp
+++ LSN_9/9.1/main.cpp
@@ -21,16 +21,17 @@ int main(int argc, char *argv[]){
 
     //Read input parameters for the algorithm
     ReadInput.open("input.in");
-    ReadInput >> CircleSquare;                  // Put the cities on a circle or on a square
-    ReadInput >> M;                             // Number of chromosomes, i.e. number of initial solutions
-    ReadInput >> NumberOfGenerations;           // Number of generations
-    ReadInput >> probSwapMutation;              // probability of performing a SWAP mutation
-    ReadInput >> probShiftMutation;             // probability of performing a SHIFT mutation
-    ReadInput >> probPermutationMutation;       // probability of performing a PERMUTATION mutation
-    ReadInput >> probInversionMutation;         // probability of performing a INVERSION mutation
-    ReadInput >> probCrossover;                 // probability of performing a CROSSOVER
-    ReadInput >> p;                             // Exponenent of the loaded die
+    ReadInput >> CircleSquare;                                                      // Put the cities on a circle or on a square
+    ReadInput >> M;                                                                 // Number of chromosomes, i.e. number of initial solutions
+    ReadInput >> NumberOfGenerations;                                               // Number of generations
+    ReadInput >> probSwapMutation;                                                  // probability of performing a SWAP mutation
+    ReadInput >> probShiftMutation;                                                 // probability of performing a SHIFT mutation
+    ReadInput >> probPermutationMutation;                                           // probability of performing a PERMUTATION mutation
+    ReadInput >> probInversionMutation;                                             // probability of performing a INVERSION mutation
+    ReadInput >> probCrossover;                                                     // probability of performing a CROSSOVER
+    ReadInput >> p;                                                                 // Exponenent of the loaded die
 
+    // User friendly messages
     cout << "\nProbability of CROSSOVER: " << probCrossover * 100 << "%" << endl;
     cout << "Probability of SWAP mutation: " << probSwapMutation * 100 << "%" << endl;
     cout << "Probability of SHIFT mutation: " << probShiftMutation * 100 << "%" << endl;
@@ -44,10 +45,12 @@ int main(int argc, char *argv[]){
     // GA constructor
     GeneticAlgorithm *GA = new GeneticAlgorithm(rnd,CircleSquare, NumberOfGenerations, M, probSwapMutation, probShiftMutation, probPermutationMutation, probInversionMutation, probCrossover, p);
 
+    // GA initialization
     cout << "\n\nInitializing population and cities..";
     GA->Start();
     cout << " DONE" <<endl;
     
+    // GA evolution
     cout << "Evolve"<<endl;
     GA->Evolve();
     cout << "DONE"<<endl;
