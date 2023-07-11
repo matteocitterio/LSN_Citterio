ef26e3108e574dfcfd305e9f225de6ebf9a06e41
matteocitterio <72971538+matteocitterio@users.noreply.github.com>
2023-07-11 14:56:32 +0200

    Add files via upload

--- LSN_8/8.2/annealing.cpp
+++ LSN_8/8.2/annealing.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 
-Annealing::Annealing(TrialWaveFunction *TWF, Random *rnd, DoubleDwellPotential *DDP){
+Annealing::Annealing(TrialWaveFunction *TWF, Random *rnd, DoubleDwellPotential *DDP) {
+
+    /*
+    Constructor of the Annealing class
+    Inputs:
+    - TrialWaveFunction *TWF: pointer to a trial wave function object, used to access all its methods.
+    - Random *rnd: pointer to the Random class
+    - DoubleDwellPotential *DDP: pointer to the function object which represents the potential I am using
+    */
+
     m_beta = 1;
     m_energy = 1;
     m_currentEnergy = m_energy;
@@ -14,17 +23,17 @@ Annealing::Annealing(TrialWaveFunction *TWF, Random *rnd, DoubleDwellPotential *
     m_L = 0;
 }
 
-void  Annealing::MetropolisAnnealing(){
+void  Annealing::MetropolisAnnealing() {
 
-    // Save the old parameters
-    double oldMu = m_f->Get_Mu();
+    /*
+    Carries out the Metropolis evolution for the simulated annealing (Not the pdf sampling)
+    */
+
+    double oldMu = m_f->Get_Mu();                                                   // Save the old parameters that will be changed if the Metropolis step is admissable
     double oldSigma = m_f->Get_Sigma();
 
-    // Update the parameters:
-    m_f->Set_Mu(abs(oldMu + m_rnd->Rannyu(-1, 1) * .5 * (1/m_beta)));
+    m_f->Set_Mu(abs(oldMu + m_rnd->Rannyu(-1, 1) * .5 * (1/m_beta)));               // Update the parameters of the trial wave function: this is the proposed move
     m_f->Set_Sigma(abs(oldSigma + m_rnd->Rannyu(-1, 1) * .25 * (1/m_beta)));
-
-    //cout << "Trying new config: MU=" << m_f->Get_Mu() << " SIGMA= " << m_f->Get_Sigma() << endl;
 
     // variables for the blocking average
     double runningSum = 0.;
@@ -32,44 +41,41 @@ void  Annealing::MetropolisAnnealing(){
     double error = 0.;
     double integral = 0.;
 
-    m_f->Equilibrate(100, pow(10, 3));
+    m_f->Equilibrate(100, pow(10, 3));                                              // Equilibrate the system for the given trial wave function
 
-    for (int i = 0; i < m_N; i++){
+    for (int i = 0; i < m_N; i++) {                                                 // Loop over the number of blocks
+
         integral = 0;
 
-        for (int j = 0; j < m_L; j++){
+        for (int j = 0; j < m_L; j++){                                              // Loop over the number of throws in each block
 
-            // updates my position according to my trial wave function distribution
-            m_f->MetropolisUniform();
-            // Now we need to compute the energy
+            m_f->MetropolisUniform();                                               // updates my position according to my trial wave function distribution and the Metropolis evolution
             integral += ((-0.5 * m_f->SecondDerivative(m_f->Get_Position())) / m_f->EvaluateNoModulus(m_f->Get_Position())) + m_d->Evaluate(m_f->Get_Position());
+
         }
+
+        // Data - blocking, I/O managment
         integral /= m_L;
-        runningSum = ((runningSum * i) + integral) / (i + 1); // at every iteration I have to re-multiplicate the previous division
+        runningSum = ((runningSum * i) + integral) / (i + 1);                       // at every iteration I have to re-multiplicate the previous division
         runningSquared = ((runningSquared * i) + pow(integral, 2)) / (i + 1);
         error = Error(runningSum, runningSquared, i);
-        //cout << "Running sum:" << runningSum<< endl;
+
     }
 
     m_currentEnergy = runningSum;
     m_currentError = error;
 
-    //cout << "OLD ENERGY=" << m_energy << " NEW ENERGY=" << m_currentEnergy << endl;
-    //cout << " exponential " << exp(-1 * m_beta * (m_currentEnergy - m_energy)) << endl;
+    double alpha = min(1., (exp(- m_beta * (m_currentEnergy - m_energy))));         // Check wheter the proposed moved is acceptable through Metropolis
 
-    //Now we perform Metropolis:
-    double alpha = min(1., (exp(- m_beta * (m_currentEnergy - m_energy))));
-
-    //cout << "ALPHA: " << alpha << endl;
-    // accepting the configuration with probability \alpha (this means rejecting it with probability (1-\alpha)):
+    // Accepting the configuration with probability \alpha (this means rejecting it with probability (1-\alpha)):
     double p = m_rnd->Rannyu();
 
-    if (alpha > p){
+    if (alpha > p){                                                                 // Hold the computed configuration
 
         m_energy = runningSum;
     }
 
-    else                      //Get back to old configuration
+    else                                                                            //Get back to old configuration
     {
         m_f ->Set_Mu(oldMu);
         m_f-> Set_Sigma(oldSigma);
--- LSN_8/8.2/annealing.h
+++ LSN_8/8.2/annealing.h
@@ -13,11 +13,16 @@ using namespace arma;
 
 class Annealing{
 
-    public:
-        Annealing(TrialWaveFunction *TWF, Random *rnd, DoubleDwellPotential *DDP); // empty constructor
-        ~Annealing(){;};    //empty destructor
+    /*
+    This class takes care of the simulated annealing schema for a trial wave function and an input dwell-shaped potential
+    */
 
-        void MetropolisAnnealing();
+    public:
+        Annealing(TrialWaveFunction *TWF, Random *rnd, DoubleDwellPotential *DDP);  // empty constructor
+        ~Annealing(){;};                                                            //empty destructor
+
+        void MetropolisAnnealing();                                                 // Metropolis algorithm for the parameters space
+
         void SetBeta(double Beta) {m_beta = Beta;}
         void SetEnergy(double Energy) { m_energy = Energy; }
         void SetN(int N) {m_N=N;}
@@ -30,14 +35,15 @@ class Annealing{
         double GetSigma() {return m_f->Get_Sigma();} 
 
     private : 
-        double m_beta;
-        double m_energy;
-        double m_currentEnergy, m_currentError;
-        int m_N;
-        int m_L;
-        TrialWaveFunction *m_f;
-        Random *m_rnd;
-        DoubleDwellPotential *m_d;
+
+        double m_beta;                                                              // Temperature of the Simulated annealing schema
+        double m_energy;                                                            // Energy of the accepted configuration
+        double m_currentEnergy, m_currentError;                                     // Energy of the proposed moved (not necessarily accepted) and relative error
+        int m_N;                                                                    // Number of blocks
+        int m_L;                                                                    // Number of throws
+        TrialWaveFunction *m_f;                                                     // Trial wave function object
+        Random *m_rnd;                                                              // Pointer to the random class
+        DoubleDwellPotential *m_d;                                                  // Potential object
 };
 
 
